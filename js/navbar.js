b61b01b6718d5f8206c062cfb805d8e24bd87618
Sam Johnson-Lacoss <99389146+samgjl@users.noreply.github.com>
2024-09-16 16:05:01 -0700

    navbar - continued modification

--- js/navbar.js
+++ js/navbar.js
@@ -7,9 +7,26 @@ class NavBar extends HTMLElement {
         this.innerHTML = `
         <nav class="navbar">
             <link rel="stylesheet" href="styles/navbar.css">
-            <a id="index" href="./"> <img src="static/space-invader.png"><span>samgjl</span> </a>
-            <a id="projects" class='nav-item' href="./projects">Projects</a>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+            <a id="index" href="./"> 
+                <img src="static/space-invader.png">
+                <span class="samgjl">samgjl</span> 
+                <a href="javascript:void(0);" class="hamburger" onclick="hamburger()">
+                    <i class="fa fa-bars"></i>
+                </a>
+            <div class="links">
+                <a id="projects" class='nav-item' href="./projects">Projects</a>
+            </div>
         </nav>`;
+    }
+}
+
+function hamburger() {
+    let links = document.querySelector(".links");
+    if (links.style.display === "block") {
+        links.style.display = "none";
+    } else {
+        links.style.display = "block";
     }
 }
 
