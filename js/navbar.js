bbe8d9812c705b67f283ab8bafeda3dfc2b88974
Sam Johnson-Lacoss <99389146+samgjl@users.noreply.github.com>
2024-09-16 16:51:08 -0700

    Navbar - 1.0

--- js/navbar.js
+++ js/navbar.js
@@ -8,14 +8,17 @@ class NavBar extends HTMLElement {
         <nav class="navbar">
             <link rel="stylesheet" href="styles/navbar.css">
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-            <a id="index" href="./"> 
+            <a id="index" href="./">
                 <img src="static/space-invader.png">
                 <span class="samgjl">samgjl</span> 
                 <a href="javascript:void(0);" class="hamburger" onclick="hamburger()">
                     <i class="fa fa-bars"></i>
                 </a>
-            <div class="links">
+            <div class="links collapsed">
                 <a id="projects" class='nav-item' href="./projects">Projects</a>
+                <a id="github" class='nav-item' href="https://github.com/samgjl" target="_blank">
+                    Github <i class="fa fa-external-link"></i>
+                </a>
             </div>
         </nav>`;
     }
@@ -23,11 +26,18 @@ class NavBar extends HTMLElement {
 
 function hamburger() {
     let links = document.querySelector(".links");
-    if (links.style.display === "block") {
-        links.style.display = "none";
+    if (links.classList.contains("expanded")) {
+        links.classList.remove("expanded");
+        links.classList.add("collapsed");
     } else {
-        links.style.display = "block";
+        links.classList.remove("collapsed");
+        links.classList.add("expanded");
     }
+    // if (links.style.display === "block") {
+    //     links.style.display = "none";
+    // } else {
+    //     links.style.display = "block";
+    // }
 }
 
 customElements.define('nav-bar', NavBar);
