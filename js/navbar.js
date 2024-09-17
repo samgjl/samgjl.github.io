94b94b86b4b4a7d45d1e7f5a82846cbedd00d596
Sam Johnson-Lacoss <99389146+samgjl@users.noreply.github.com>
2024-09-16 22:48:40 -0700

    Game of life - beginning

--- js/navbar.js
+++ js/navbar.js
@@ -10,17 +10,25 @@ class NavBar extends HTMLElement {
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
             <a id="index" href="./">
                 <img src="static/space-invader.png">
-                <span class="samgjl">samgjl</span> 
+                <span id="samgjl" class="samgjl">samgjl</span> 
                 <a href="javascript:void(0);" class="hamburger" onclick="hamburger()">
                     <i class="fa fa-bars"></i>
                 </a>
             <div class="links collapsed">
                 <a id="projects" class='nav-item' href="./projects">Projects</a>
+                <a id="about" class='nav-item' href="./about">About</a>
                 <a id="github" class='nav-item' href="https://github.com/samgjl" target="_blank">
                     Github <i class="fa fa-external-link"></i>
                 </a>
             </div>
         </nav>`;
+    }
+
+    setActive(id) {
+        let target = this.querySelector(`#${id}`);
+        if (target) {
+            target.classList.add("active");
+        }
     }
 }
 customElements.define('nav-bar', NavBar);
